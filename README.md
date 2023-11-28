aa692eb581c4cab954863203d2f6083f3ffd6bf2
Tongcai618 <cai.to@northeastern.edu>
2023-11-27 23:45:50 -0500

    Website with sidebar, home

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# Tong's Personal Website
-
-To access the website, please click: https://tongcai618.github.io/Tong-Personal-Website/
+# JavaScript-Full-Course
+A JavaScript Full course
---
+++ react-portfolio/src/components/Home/index.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-T.png';
+import './index.scss';
+
+const Home = () => {
+
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm 
+                <img src={LogoTitle} alt="developer"/>
+                ong
+                <br />
+                Programer
+                </h1>
+                <h2>NEU Student / Full-Stack Developer</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
