8f58e16c74aea1b536d9a16190576678b77005a3
Tongcai618 <tongcai010618@gmail.com>
2023-12-01 23:14:53 -0500

    Website Update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-T.png';
 import './index.scss';
+import Loader from 'react-loaders'
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import Logo from './Logo'
@@ -14,11 +15,11 @@ const Home = () => {
         const timer = setTimeout(() => {
             setLetterClass('text-animate-hover')
         }, 4000);
-        // Cleanup function
         return () => clearTimeout(timer);
     }, [])
 
     return (
+        <>
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
@@ -34,13 +35,16 @@ const Home = () => {
                     <br />
                     <AnimatedLetters letterClass={letterClass}
                         strArray={jobArray}
-                        idx={19} />
+                        idx={19}
+                    />
                 </h1>
                 <h2>NEU Student / Full-Stack Developer</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
             <Logo />
-        </div>
+            </div>
+            <Loader type="pacman" />
+            </>
     )
 }
 
